ab2e9ddd543a5ac3422dd6c3286343be46dd3155
lmStormz <boulligny.vincentpro@gmail.com>
2022-02-07 21:13:18 +0100

    test jour

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const { Client, Intents } = require('discord.js');
 const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const prefix="!";
 
-//import mwclient
-//site =mwclient.Site('lol.fandom.com', path='/')
+//var leaguepedia=require('mwclient');
+//leaguepedia =mwclient.Site('lol.fandom.com', path='/')
 
 
 var d = new Date();
+const jour= new Array[7];
+jour[0]="Lundi";
+jour[1]="Mardi";
+jour[2]="Mercredi";
+jour[3]="Jeudi";
+jour[4]="Vendredi";
+jour[5]="Samedi";
+jour[6]="Dimanche";
+
 
 
 client.on("message", function(message) {
@@ -30,7 +39,7 @@ client.on("message", function(message) {
   }
 
   else if (command === "date") {
-    const jour = d.getDay();
+     jour[] = d.getDay();
     message.reply(`On est ${jour}!`);
   }
 
